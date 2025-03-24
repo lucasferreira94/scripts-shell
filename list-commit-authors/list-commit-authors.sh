5e9a460ca9fc39e7eb62121dd51cd0fecdb5c12e
Lucas Santos Ferreira <45995734+lucasferreira94@users.noreply.github.com>
2025-03-24 16:53:17 -0300

    Create list-commit-authors.sh

---
+++ list-commit-authors/list-commit-authors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# -------------------------- HEADER --------------------------
+#
+#   List all commits authors in all cloned repos
+#   v1.0
+#
+#   How to use:
+#   Execute the script "./list-commit-authors.sh" into a directory which have all repos cloned into it 
+#   Then, verify the content of file "commit-users.txt" to check how many users commited into your projects
+#
+# -------------------------------------------------------------
+
+GIT_DIRS="$(find -name '.git')"
+
+for i in ${GIT_DIRS}; do
+    cd $i
+    git log | grep Author | sort | uniq >> ${PWD}/commit-users.txt
+    cd - 
+done
+
+printf "\n"
+echo "Arquivo exportado! confira a lista de users commit em: ${PWD}/commit-users.txt"
